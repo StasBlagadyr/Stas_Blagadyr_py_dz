c8c0b59a23f5805f611bad068e8c0151c2199c1b
Stas <stas.blagadyr.93@gmail.com>
2021-09-12 20:47:13 +0300

    DZ_2

---
+++ Home_Dz2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	//Task 1
+	{
+		short int a = 30000;
+		int b = 300'000'000;
+		long long c = 46541871986768414;
+		char d = 'S';
+		bool e = true;
+		float f = 3.4E+38;
+		double g = 1.7E+308;
+
+		return 0;
+	}
+	//Task2
+	{
+		enum MyGame{x, o ,pass};
+		
+		//Task3
+		MyGame arrival[9] = { x,pass,o,x,pass,pass,o,x,pass };
+	}
+	
+	
+}
